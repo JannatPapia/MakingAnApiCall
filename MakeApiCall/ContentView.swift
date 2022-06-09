f8ee62e2dbc6ec8c92472e11d2b7b17b3ac0d4d9
PapiaMCode <papiamcode@gmail.com>
2022-06-09 02:35:46 -0700

    Create Class

--- MakeApiCall/ContentView.swift
+++ MakeApiCall/ContentView.swift
@@ -11,7 +11,18 @@ struct ContentView: Codable, Identifiable {
     let id = UUID()
     var title: String
     var body: String
-  //stopit 
+  //stopit
 }
 
-
+class Api {
+    func getPosts() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            let posts = try! JSONDecoder().decode([ContentView].self, from: data!)
+            print(posts)
+        }
+        .resume()
+    }
+}
