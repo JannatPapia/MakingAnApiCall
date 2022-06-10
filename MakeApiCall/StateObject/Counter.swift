fac919ffdacae4a6ea5dc8a0d544321d08631551
PapiaMCode <papiamcode@gmail.com>
2022-06-10 00:54:50 -0700

    Understanding @StateObject

--- MakeApiCall/StateObject/Counter.swift
+++ MakeApiCall/StateObject/Counter.swift
@@ -7,9 +7,43 @@
 
 import SwiftUI
 
-struct Counter: View {
+class Count: ObservableObject {
+    @Published var value : Int = 0
+}
+
+struct CounterView : View {
+    
+    //   @ObservedObject var counter = Count()
+    @StateObject var counter = Count()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("\(counter.value)")
+            Button("CountentView Value Increment") {
+                counter.value += 1
+                
+            }
+        }.padding()
+            .background(Color.green)
+  //      Text("Counter View")
+    }
+}
+
+struct Counter : View {
+    
+    @State private var count : Int = 0
+    var body: some View {
+        VStack {
+        Text("COUNTER VIEW")
+            Text("\(count)")
+            Button("Increment Counter") {
+                count += 1
+            }
+            CounterView()
+        }
+        .padding()
+                .background(Color.yellow)
+        
     }
 }
 
